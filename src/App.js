e24aacd86d2fb04ec8ee6228b95e65de01964e39
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2019-08-13 14:16:51 -0300

    Atualizado logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Bancos Brasileiros - Builder widget</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add bank</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View banks</h2>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
