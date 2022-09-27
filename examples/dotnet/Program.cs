8441e863afe97145aad5bb90cb6cbe6d0430066c
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2022-09-27 18:18:29 +0400

    Upgrade examples (#148)
    * Python schema: Ensure datetime fields is converted to datetime type
    
    * Create run.yml
    
    * Guibranco patch 1 (#138)
    
    * Python schema: Ensure datetime fields is converted to datetime type
    
    * Create run.yml
    
    * Update run.yml
    
    Co-authored-by: Leonardo Gregianin <leogregianin@gmail.com>
    
    * Update run.yml (#140)
    
    * Update README.md
    
    * Update README.md
    
    * Update documentation with new fields (#144)
    
    * Closes #143
    
    * Update schemas
    
    * Upgrade examples (#147)
    
    * Add more properties examples
    
    * Update package version and .net version
    
    * Update emberJS example
    
    * Add more fields to example
    
    Co-authored-by: Leonardo Gregianin <leogregianin@gmail.com>

--- examples/dotnet/Program.cs
+++ examples/dotnet/Program.cs
@@ -14,6 +14,19 @@
             var banks = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Bank>>(fileContent);
 
             Console.WriteLine($"Banks: {banks.Count}");
+            foreach(var bank in banks)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($"{bank.Compe}\t[CNPJ: {bank.Document}]\t{bank.LongName}");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine($"\tTipo: {bank.Type ?? ""}");
+                Console.WriteLine($"\tBoleto: {bank.Charge??false}");
+                Console.WriteLine($"\tTED/DOC: {bank.CreditDocument??false}");
+                Console.WriteLine($"\tPIX: {bank.PixType??"False"}");
+                Console.WriteLine($"\tPortabilidade: {bank.SalaryPortability??"False"}");
+                Console.WriteLine($"\tAtualizado em: {bank.DateUpdated}");
+            }
             Console.ReadKey();
         }
     }
