21df7a1607c065a545d99a2a3b5e4d56600d2ab4
amol <amlo1232001@gmail.com>
2023-06-30 01:03:43 +0530

    refactor: src/Bank

---
+++ schemas/php.php
@@ -0,0 +1,55 @@
+<?php
+
+class Bank
+{
+    /** @var string */
+    public $COMPE;
+
+    /** @var string */
+    public $ISPB;
+
+    /** @var string */
+    public $Document;
+
+    /** @var string */
+    public $LongName;
+
+    /** @var string */
+    public $ShortName;
+
+    /** @var string|null */
+    public $Network;
+
+    /** @var string|null */
+    public $Type;
+
+    /** @var string|null */
+    public $PixType;
+
+    /** @var bool|null */
+    public $Charge;
+
+    /** @var bool|null */
+    public $CreditDocument;
+
+    /** @var string|null */
+    public $SalaryPortability;
+
+    /** @var string[]|null */
+    public $Products;
+
+    /** @var string|null */
+    public $Url;
+
+    /** @var string|null */
+    public $DateOperationStarted;
+
+    /** @var string|null */
+    public $DatePixStarted;
+
+    /** @var DateTime|string */
+    public $DateRegistered;
+
+    /** @var DateTime|string */
+    public $DateUpdated;
+}
