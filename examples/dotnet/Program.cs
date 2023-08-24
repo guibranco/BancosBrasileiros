8c63887c1eb88beb205e0d135002c241c7e1b92c
Maurici Dmarco <129069676+MauriciDmarc@users.noreply.github.com>
2023-08-24 14:13:41 -0300

    Implementado filtro de busca baseado nos 3 dígitos do código COMPE - Examplo .NET (#286)
    * //// Implementado filtro de busca baseado nos 3 dígitos do código COMPE
    
    * #Rename filterBanks to FilterBanks

--- examples/dotnet/Program.cs
+++ examples/dotnet/Program.cs
@@ -3,16 +3,15 @@
     using BancosBrasileiros;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     class Program
     {
-        static void Main(string[] _)
+        private static string fileContent = System.IO.File.ReadAllText("banks.json");
+        private static List<Bank> banks = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Bank>>(fileContent);
+        static void Main()
         {
             Console.WriteLine("Hello World!");
-
-            var fileContent = System.IO.File.ReadAllText("banks.json");
-            var banks = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Bank>>(fileContent);
-
             Console.WriteLine($"Banks: {banks.Count}");
             foreach(var bank in banks)
             {
@@ -25,9 +24,57 @@
                 Console.WriteLine($"\tTED/DOC: {bank.CreditDocument??false}");
                 Console.WriteLine($"\tPIX: {bank.PixType??"False"}");
                 Console.WriteLine($"\tPortabilidade: {bank.SalaryPortability??"False"}");
-                Console.WriteLine($"\tAtualizado em: {bank.DateUpdated}");
+                Console.WriteLine($"\tAtualizado em: {bank.DateUpdated}\n");
             }
-            Console.ReadKey();
+
+            Console.Write("Buscar COMPE (3 dígitos): ");
+            var compe = Console.ReadLine();
+
+            FilterBanks(compe);
+        }
+
+        private static void FilterBanks(string compe)
+        {
+            Console.Clear();
+
+            var sortedBanks = banks.Where(b => b.Compe == compe);
+
+            if (sortedBanks.Count() != 0) 
+            {
+                foreach (var bank in sortedBanks)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"{bank.Compe}\t[CNPJ: {bank.Document}]\t{bank.LongName}");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    Console.WriteLine($"\tTipo: {bank.Type ?? ""}");
+                    Console.WriteLine($"\tBoleto: {bank.Charge ?? false}");
+                    Console.WriteLine($"\tTED/DOC: {bank.CreditDocument ?? false}");
+                    Console.WriteLine($"\tPIX: {bank.PixType ?? "False"}");
+                    Console.WriteLine($"\tPortabilidade: {bank.SalaryPortability ?? "False"}");
+                    Console.WriteLine($"\tAtualizado em: {bank.DateUpdated} \n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nenhum Resultado Encontrado.");
+            }
+
+            Console.Write("1.Listar Todos \t 2.Buscar COMPE: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            if(option == 1)
+            {
+                Console.Clear();
+                Main();
+            }
+            if(option == 2)
+            {
+                Console.Clear();
+                Console.Write("Buscar COMPE (3 dígitos): ");
+                compe = Console.ReadLine();
+                FilterBanks(compe);
+            }
         }
     }
 }
