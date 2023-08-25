393e27433988c28b70327bd733b1c25c4988c3bd
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-08-25 11:07:31 +0100

    Linter

--- examples/dotnet/Program.cs
+++ examples/dotnet/Program.cs
@@ -8,22 +8,25 @@
     class Program
     {
         private static string fileContent = System.IO.File.ReadAllText("banks.json");
-        private static List<Bank> banks = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Bank>>(fileContent);
+        private static List<Bank> banks = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Bank>>(
+            fileContent
+        );
+
         static void Main()
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine($"Banks: {banks.Count}");
-            foreach(var bank in banks)
+            foreach (var bank in banks)
             {
                 Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine($"{bank.Compe}\t[CNPJ: {bank.Document}]\t{bank.LongName}");
                 Console.ForegroundColor = ConsoleColor.White;
 
                 Console.WriteLine($"\tTipo: {bank.Type ?? ""}");
-                Console.WriteLine($"\tBoleto: {bank.Charge??false}");
-                Console.WriteLine($"\tTED/DOC: {bank.CreditDocument??false}");
-                Console.WriteLine($"\tPIX: {bank.PixType??"False"}");
-                Console.WriteLine($"\tPortabilidade: {bank.SalaryPortability??"False"}");
+                Console.WriteLine($"\tBoleto: {bank.Charge ?? false}");
+                Console.WriteLine($"\tTED/DOC: {bank.CreditDocument ?? false}");
+                Console.WriteLine($"\tPIX: {bank.PixType ?? "False"}");
+                Console.WriteLine($"\tPortabilidade: {bank.SalaryPortability ?? "False"}");
                 Console.WriteLine($"\tAtualizado em: {bank.DateUpdated}\n");
             }
 
@@ -39,7 +42,7 @@
 
             var sortedBanks = banks.Where(b => b.Compe == compe);
 
-            if (sortedBanks.Count() != 0) 
+            if (sortedBanks.Count() != 0)
             {
                 foreach (var bank in sortedBanks)
                 {
@@ -63,12 +66,12 @@
             Console.Write("1.Listar Todos \t 2.Buscar COMPE: ");
             int option = Convert.ToInt32(Console.ReadLine());
 
-            if(option == 1)
+            if (option == 1)
             {
                 Console.Clear();
                 Main();
             }
-            if(option == 2)
+            if (option == 2)
             {
                 Console.Clear();
                 Console.Write("Buscar COMPE (3 dígitos): ");
